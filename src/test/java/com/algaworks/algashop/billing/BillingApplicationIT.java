aca602ab76ee4161e49697a835169ad096f00400
thiagoaataide <thiagoaataide@gmail.com>
2026-08-21 13:46:01 -0300

    configuração de dependências e conexão com o banco de dados

--- src/test/java/com/algaworks/algashop/billing/BillingApplicationIT.java
+++ src/test/java/com/algaworks/algashop/billing/BillingApplicationIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BillingApplicationTests {
+class BillingApplicationIT {
 
 	@Test
 	void contextLoads() {
